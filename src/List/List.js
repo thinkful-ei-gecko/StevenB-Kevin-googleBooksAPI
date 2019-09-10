a89b32dae048bbeba5c5b936847037a662cbc28d
Kevin Wei <chenghaowei@gmail.com>
2019-09-10 17:54:39 -0400

    more dead code

--- src/List/List.js
+++ src/List/List.js
@@ -4,12 +4,12 @@ import './List.css';
 
 export default class List extends Component {
   render() {
-    /* const listItems = this.props.books
-      .map((book, index) => <ListItem {...book} key={index} />); */
+    const listItems = this.props.books
+      .map((book, index) => <ListItem {...book} key={index} />);
 
     return (
       <ul className="List">
-        {this.props.books.length ? this.props.books.map((book, index) => <ListItem {...book} key={index} />) : <h2>No Books!</h2> }
+        {listItems}
       </ul>
     )
   }
