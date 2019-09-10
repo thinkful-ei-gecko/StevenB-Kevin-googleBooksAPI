b9da1c35def60c1144c4b274da3b3a32082308cb
Steven Bull <stevenwbulljr@gmail.com>
2019-09-10 07:53:52 -0500

    add API documentation link to app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
 /* API Key: AIzaSyDDWElUEuq7wjg6crMFUYzphoMMB0ZEIlQ */
+// API documentation
+/* https://developers.google.com/books/docs/v1/using#PerformingSearch */
 
 function App() {
   return (
